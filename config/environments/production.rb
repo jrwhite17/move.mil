a5149d49f1ad4360930bb5c4f8c190c6692f314c
jrwhite17 <johnrileywhite@gmail.com>
2017-10-09 09:49:39 -0500

    update
    testing deployment on elastic beanstalk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,7 @@
 Rails.application.configure do
+  #Testing Secret Key Base location
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
